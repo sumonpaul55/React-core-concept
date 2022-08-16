b40d42143a565b3891982fd429666bb903975aac
sumon paul <sumpaul100@gmail.com>
2022-08-17 01:21:54 +0600

    call data from an arry and object

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const product = [
+    { name: "Photoshop cs", price: "99.44$" },
+    { name: "Photoshop cc", price: "93.44$" },
+    { name: "Photoshop cc", price: "33.44$" },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <div style={{ display: "flex" }}>
+          <Product product={product[2]}></Product>
+          <Product product={product[0]}></Product>
+          <Product product={product[1]}></Product>
+        </div>
       </header>
     </div>
   );
 }
-const Person = () => {
-  const personStyle = {
-    border: "1px solid red",
-    margin: "5px 0",
+const Product = (props) => {
+  const styleProduct = {
+    background: "lightblue",
+    padding: "15px",
+    borderRadius: "10px",
+    color: "black",
+    margin: "5px",
   };
   return (
-    <div style={personStyle}>
-      <h1>Name : This is custom components</h1>
-      <h1>Another tag</h1>
+    <div style={styleProduct}>
+      <h2>{props.product.name}</h2>
+      <h3>{props.product.price}</h3>
+      <button style={{ padding: "10px", borderRadius: "5px", border: "none" }}>Buy Now</button>
     </div>
   );
 };
