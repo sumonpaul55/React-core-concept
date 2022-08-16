bf1edc68385d1385149cbd9a13e526e8a9b0e94a
sumon paul <sumpaul100@gmail.com>
2022-08-16 20:06:45 +0600

    inline css added in react

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
 const Person = () => {
-  return <h1>Name : This is custom components</h1>;
+  // const personStyle = {
+  //   border: "1px solid red",
+  //   margin: "5px 0",
+  // };
+  return (
+    <div style={{ border: "1px solid red", marginTop: "5px" }}>
+      <h1>Name : This is custom components</h1>
+      <h1>Another tag</h1>
+    </div>
+  );
 };
 
 export default App;
